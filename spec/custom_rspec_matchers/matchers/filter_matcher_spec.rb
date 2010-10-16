8e3e27f2117a18c6a5ac782ecc49c47cd00a37b2
Matt Simpson <matt.simpson3@gmail.com>
2010-10-16 11:15:53 -0500

    Make test for FilterMatcher be more readable and arranged better

--- spec/custom_rspec_matchers/matchers/filter_matcher_spec.rb
+++ spec/custom_rspec_matchers/matchers/filter_matcher_spec.rb
@@ -1,41 +1,60 @@
 require 'spec_helper'
 
-class FooController < ActionController::Base
-  before_filter :before_for_all_methods
-  after_filter :after_for_all_methods
-
-  after_filter :after_filter_only_create, :only => :create
-  after_filter :after_filter_only_show, :only => :show
-  after_filter :after_filter_only_show_and_create, :only => [:show, :create]
-  after_filter :after_filter_except_show, :except => :show
-  after_filter :after_filter_except_create, :except => :create
-  after_filter :after_filter_except_show_and_create, :except => [:show, :create]
-
-  before_filter :before_filter_only_create, :only => :create
-  before_filter :before_filter_only_show, :only => :show
-  before_filter :before_filter_only_show_and_create, :only => [:show, :create]
-  before_filter :before_filter_except_show, :except => :show
-  before_filter :before_filter_except_create, :except => :create
-  before_filter :before_filter_except_show_and_create, :except => [:show, :create]
-end
-
-class BarController < ActionController::Base ; end
-
 describe 'FilterMatcher' do
-  before do
-    @foo = FooController.new
-    @to = CustomRspecMatchers::Matchers::FilterMatcher.new :before, :before_for_all_methods
-    @bar = BarController.new
-  end
-  
-  it { @to.description.should == "include before filter: before_for_all_methods" }
-  it { @to.matches?(@foo).should == true }
-  it { @foo.should include_before_filter(:before_for_all_methods) }
-  
-  describe 'failure message' do
+  describe "matcher for a before filter" do
     before do
-      @to.matches?(@bar)
+      @filter_under_test = :some_before_filter
+      @matcher = FilterMatcher.new :before, @filter_under_test
     end
-    it { @to.failure_message.should == " expected to include before filter before_for_all_methods but not found in []" }
+    
+    it "has the correct description" do
+      @matcher.description.should == "include before filter: some_before_filter"
+    end
+    
+    describe "calling matches? on the matcher with a controller that doesn't have any filters" do
+      before do
+        class TestController < ActionController::Base; end
+        @test_object = @matcher.matches? TestController.new
+      end
+      
+      it "returns false" do
+        @test_object.should == false
+      end
+      it "has the correct failure message" do
+        @matcher.failure_message.should == " expected to include before filter some_before_filter but not found in []"
+      end
+    end
+    
+    describe "calling matches? on the matcher with a controller that doesn't have any before filters" do
+      before do
+        class TestController < ActionController::Base
+          after_filter :awesome_after_filter
+        end
+        @test_object = @matcher.matches? TestController.new
+      end
+      
+      it "returns false" do
+        @test_object.should == false
+      end
+      it "has the correct failure message" do
+        @matcher.failure_message.should == " expected to include before filter some_before_filter but not found in []"
+      end
+    end
+    
+    describe "calling matches? on the matcher with a controller that doesn't have the before filter" do
+      before do
+        class TestController < ActionController::Base
+          before_filter :awesome_after_filter
+        end
+        @test_object = @matcher.matches? TestController.new
+      end
+      
+      it "returns false" do
+        @test_object.should == false
+      end
+      it "has the correct failure message" do
+        @matcher.failure_message.should == " expected to include before filter some_before_filter but not found in [:awesome_after_filter]"
+      end
+    end
   end
 end
